12af82523a5b9cf66f8deb1a1b9a76c6c076c1ec
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-28 13:06:12 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The root Tree",
+	id: "rootTreemod",
+	author: "Mason",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
